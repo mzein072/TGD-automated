c86f26858d7e6d66f11377e6e87be97de3f88f4e
Mohammad Zein Aghaji <mohammadzeinaghaji@gmail.com>
2021-12-21 09:05:01 -0500

    Update PEC_calculator.vb

--- TGD_alternate_beta/PEC_calculator.vb
+++ TGD_alternate_beta/PEC_calculator.vb
@@ -2,6 +2,8 @@ Dim CB_Val As Variant
 Private Sub ComboBox1_change()
 'DROP-DOWN USE CATEGORY BOX ///////////////////////////////////////////////////////
 
+Worksheets("PEC Calculator").Unprotect "eau"
+
 If Me.ComboBox1.Value <> CB_Val Then
        
     CB_Val = Me.ComboBox1.Value
@@ -16,10 +18,27 @@ If Me.ComboBox1.Value <> CB_Val Then
     End If
 
 End If
+
+Worksheets("PEC Calculator").Protect "eau"
+
 End Sub
 
 Private Sub Worksheet_Change(ByVal Target As Range)
+
+Worksheets("PEC Calculator").Unprotect "eau"
+
 Application.EnableEvents = False
+
+
+
+If Worksheets("PEC Calculator").Range("D12").Value = "" Then
+Worksheets("PEC Calculator").Range("D12").Value = 0
+End If
+
+
+If Worksheets("PEC Calculator").Range("D13").Value = "" Then
+Worksheets("PEC Calculator").Range("D13").Value = 100
+End If
 
 '///////////////////////////////////////SHEET INITIALIZATION ///////////////////////////////////////////////////////////////////
     If Target.Address = "$D$3" Then
@@ -31,17 +50,18 @@ Application.EnableEvents = False
             Range("D5").Value = ""
             ComboBox1.Value = ""
             ComboBox1.ListFillRange = "UC_List"
-            Range("C22").Value = "Please Select"
-            Range("C25").Value = "Please Select"
-            Range("C28").Value = "Not Applicable"
+            Range("C23").Value = "Please Select"
+            Range("C26").Value = "Please Select"
+            Range("C29").Value = "Not Applicable"
             'Reset prod values too
             Range("D11").Value = ""
-            Range("D12").Value = ""
-            Range("D13").Value = ""
+            Range("D12").Value = "0"
+            Range("D13").Value = "100"
+            Range("D14").Value = ""
             Range("D15").Value = ""
-            Range("D16").Value = ""
             Range("D17").Value = ""
             Range("D18").Value = ""
+            Range("D19").Value = ""
         Else
             Range("D7").Value = ""
             Range("D6").Value = ""
@@ -49,63 +69,77 @@ Application.EnableEvents = False
             Range("D5").Value = ""
             ComboBox1.Value = ""
             ComboBox1.ListFillRange = "UC_List"
-            Range("C22").Value = "Please Select"
-            Range("C25").Value = "Please Select"
-            Range("C28").Value = "Please Select"
+            Range("C23").Value = "Please Select"
+            Range("C26").Value = "Please Select"
+            Range("C29").Value = "Please Select"
             'Reset prod values too
             Range("D11").Value = ""
-            Range("D12").Value = ""
-            Range("D13").Value = ""
+            Range("D12").Value = "0"
+            Range("D13").Value = "100"
+            Range("D14").Value = ""
             Range("D15").Value = ""
-            Range("D16").Value = ""
             Range("D17").Value = ""
             Range("D18").Value = ""
+            Range("D19").Value = ""
         End If
     End If
-    
+ 
+If Worksheets("PEC Calculator").Range("D11").Value = "1 facility" Then
 Call TableSources
 
-Call BTables
+Call BTables_1F
 
-Call ATables
+Call ATables_1F
+Call ATables_1FB
 
-Call ATables2
 
+ElseIf Worksheets("PEC Calculator").Range("D11").Value = "Multiple facilities" Then
+Call TableSources
 
+Call BTables_Multiple
+
+Call ATables_Multiple
+
+End If
 
 
 Application.EnableEvents = True
+
+Worksheets("PEC Calculator").Protect "eau"
+
 End Sub
 
 Private Sub Worksheet_Calculate()
 'https://stackoverflow.com/questions/40741967/run-macro-when-linked-cell-changes-value-excel-vba
+Worksheets("PEC Calculator").Unprotect "eau"
+
 
 Application.EnableEvents = False
 
     If Me.Range("D9").Value = "Cosmetics" Then
         
             If Range("D3").Value = "Public Domain" Then
-            Range("C22").Value = "Not Applicable"
-            Range("C25").Value = "Please Select"
-            Range("C28").Value = "Not Applicable"
+            Range("C23").Value = "Not Applicable"
+            Range("C26").Value = "Please Select"
+            Range("C29").Value = "Not Applicable"
             End If
 
                     
         ElseIf Me.Range("D9").Value = "Cleaning/washing agents and additives" Then
         
             If Range("D3").Value = "Public Domain" Then
-            Range("C22").Value = "Not Applicable"
-            Range("C25").Value = "Not Applicable"
-            Range("C28").Value = "Not Applicable"
+            Range("C23").Value = "Not Applicable"
+            Range("C26").Value = "Not Applicable"
+            Range("C29").Value = "Not Applicable"
             End If
 
                    
         ElseIf Me.Range("D9").Value = "Colouring agents" Then
         
             If Range("D3").Value = "Leather Processing" Then
-            Range("C22").Value = "Please Select"
-            Range("C25").Value = "Not Applicable"
-            Range("C28").Value = "Not Applicable"
+            Range("C23").Value = "Please Select"
+            Range("C26").Value = "Not Applicable"
+            Range("C29").Value = "Not Applicable"
             End If
     
         
@@ -113,9 +147,9 @@ Application.EnableEvents = False
             Me.Range("D9").Value <> "Cosmetics" Or Me.Range("D9").Value <> "Colouring agents" Then
             
             If Range("D3").Value = "Public Domain" Then
-            Range("C22").Value = "Please Select"
-            Range("C25").Value = "Please Select"
-            Range("C28").Value = "Not Applicable"
+            Range("C23").Value = "Please Select"
+            Range("C26").Value = "Please Select"
+            Range("C29").Value = "Not Applicable"
             End If
             
         End If
@@ -123,12 +157,13 @@ Application.EnableEvents = False
 If Me.Range("D9").Value <> CB_Val Then
         'Reset prod values too
             Range("D11").Value = ""
-            Range("D12").Value = ""
-            Range("D13").Value = ""
+            Range("D12").Value = 0
+            Range("D13").Value = 100
+            Range("D14").Value = ""
             Range("D15").Value = ""
-            Range("D16").Value = ""
             Range("D17").Value = ""
             Range("D18").Value = ""
+            Range("D19").Value = ""
             
     If ComboBox1.Value <> "" Then
     ComboBox1.ListFillRange = "UC_List"
@@ -142,8 +177,13 @@ If Me.Range("D9").Value <> CB_Val Then
     CB_Val = Me.Range("D9").Value
     
 End If
-    
+
+
 Application.EnableEvents = True
+
+Worksheets("PEC Calculator").Protect "eau"
+
+
 End Sub
 
 
